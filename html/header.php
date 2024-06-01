0e72a6447a4f0841613f86ec68db1f7687f40267
wilfredstewart <160639787+wilfredstewart@users.noreply.github.com>
2024-06-01 23:59:50 +0530

    project lourdes

---
+++ html/header.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LHS</title>
+
+    <meta property="og:type" content="website" />
+    <meta property="website:publisher" content="https://kvmmissionindia.org" />
+    <meta property="website:section" content="KVM Helping details here" />
+    <meta property="website:tag" content="KVM" />
+    <meta property="og:image" content="https://sample image of kvm " />
+    <meta property="og:image:secure_url" content="https://sample image of kvm" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="720" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:alt" content="Your Full Service Marketing Specialists" />
+    <meta property="og:locale" content="en_GB" />
+    <link rel="canonical" href="https://kvmmissionindia.org" />
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/lhs_logo.png">
+    <link rel="apple-touch-icon" href="../images/lhs_logo.png" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/css/uikit.min.css" />
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Walter+Turncoat&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Platypi:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/index.css">
+
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.18.3/dist/js/uikit-icons.min.js"></script>
+</head>
+<body>
+<div class="lhs-navbar"
+        uk-sticky="start: 170; animation: uk-animation-slide-top; sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; cls-inactive: uk-navbar-transparent uk-light; end: ! *; offset: 0">
+
+        <nav class="uk-navbar-container uk-light">
+            <div class="uk-container uk-container-large">
+                <div class="uk-navbar uk-margin-top">
+                    <div class="uk-navbar-left">
+                        <a class="uk-navbar-item uk-logo lhs_logo" href="#slider" uk-scroll>
+                            <img src="../images/lhs_logo.png">
+                        </a>
+                    </div>
+                    <div class="uk-navbar-right ">
+                        <ul class="uk-navbar-nav lhs-navBg uk-flex-middle"
+                            uk-scrollspy-nav="closest: li;scroll: true; offset:50;">
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Home"></span> Home
+                                </a>
+                            </li>
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Admissions"></span> Admissions
+                                </a>
+                            </li>
+                            <!-- <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Why choose us"></span> Why choose us
+                                </a>
+                            </li> -->
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Campus Tours"></span> Campus Tours
+                                </a>
+                            </li>
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Our Community"></span> Our Community
+                                </a>
+                            </li>
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="About us"></span> About us
+                                </a>
+                            </li>
+                            <li class="">
+                                <a class="lhs-navButtonhover" href="https://wilfredstewart.github.io/lourdesschool/html/siteunderconstruction.html">
+                                    <span class="" data-text="Contact Us"></span> Contact Us
+                                </a>
+                            </li>
+                            <a href=""
+                                class="uk-navbar-toggle lhs-hidden lhs-visible uk-navbar-toggle-icon uk-margin-small-left uk-icon uk-padding-remove ds-icon-color"
+                                uk-navbar-toggle-icon="ratio: 1.3" uk-toggle="target: #offcanvas-usage"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
